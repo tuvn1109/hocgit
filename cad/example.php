eda9ce230df63add67a623b906fdb1112210bbe8
TU VN <nvtu1009@gmail.com>
2020-11-02 10:33:40 +0700

    lan 2

---
+++ cad/example.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @author Alessandro Vernassa 
+ */
+ob_start();
+require_once 'dxfwriter.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>DXF Writer</title>
+    </head>
+    <body>
+        <pre>CODE:2
+        $shape = new DXF();
+        $shape->addLayer("mylayer", DXF_COLOR_RED);
+        $shape->addLayer("text", DXF_COLOR_BLUE);
+        $shape->addText(12, 110, 0, "HELLO WORLD", 5, "text");
+        $shape->addCircle(50, 100, 0, 50, "mylayer");
+        $shape->addLine(0, 100, 0, 100, 100, 0, "mylayer");
+        </pre>A
+        <a href="?download">down22load dxf</a><br />
+        <?php
+        $shape = new DXF();
+        $shape->addLayer("mylayer", DXF_COLOR_RED);
+        $shape->addLayer("text", DXF_COLOR_BLUE);
+        $shape->addText(12, 110, 0, "HELLO WORLD", 5, "text");
+        $shape->addCircle(50, 100, 0, 50, "mylayer");
+        $shape->addLine(0, 100, 0, 100, 100, 0, "mylayer");
+        $shape->addLine(101, 100, 101, 200, 200, 101, "mylayer");
+        if (isset($_GET['download']))
+        {
+            $shape->SaveFile("myFile.dxf");        
+        }
+        $dxfstring = $shape->getString();
+        
+        echo "<h2>dxf string:</h2><pre>$dxfstring</pre>";
+        ?>
+    </body>
+</html>
